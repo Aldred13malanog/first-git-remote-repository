2734dae5002d38262b96049b9213c8837c63e2ae
Aldred <aldredmalanog@gmail.com>
2024-04-30 19:39:39 +0800

    lesson 15 codes(dont know if its working, cuz i don hav wifi)

--- JavaScript/JavaScript-Amazon-Project(lesson 13 Part 2)/data/cart.js
+++ JavaScript/JavaScript-Amazon-Project(lesson 13 Part 2)/data/cart.js
@@ -4,10 +4,12 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
 	cart = [{
 		productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-		quantity: 2
+		quantity: 2,
+		deliveryOptions: '1'
 	}, {
 		productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-		quantity: 1
+		quantity: 1,
+		deliveryOptions: '2'
 	}];
 }
 
@@ -30,7 +32,8 @@ export function addToCart(productId) {
 	} else {
 		cart.push({
 			productId: productId,
-			quantity: 1
+			quantity: 1,
+			deliveryOptions: '1'
 		});
 	}
 	saveToStorage();
@@ -47,4 +50,18 @@ export function removeFromCart(productId) {
 
 	cart = newCart;
 	saveToStorage();	
+}
+
+export function updateDeliveryOption(productId, deliverOptionId) {
+	let matchingItem;
+
+	cart.forEach((cartItem) => {
+		if (productId === cartItem.productId) {
+			matchingItem = cartItem;
+		}
+	});
+
+	matchingItem.deliverOptionId = deliverOptionId;
+
+	saveToStorage();
 }
